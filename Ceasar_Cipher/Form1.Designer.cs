2758fed086a9ede30b20c4924c611b774889c2c1
MartynasCepas <martynas.cepas@ktu.edu>
2020-10-25 17:32:31 +0200

    Added feature to decrypt message

--- Ceasar_Cipher/Form1.Designer.cs
+++ Ceasar_Cipher/Form1.Designer.cs
@@ -35,17 +35,20 @@
             this.label4 = new System.Windows.Forms.Label();
             this.trackBar1 = new System.Windows.Forms.TrackBar();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
-            this.result1 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.result1 = new System.Windows.Forms.TextBox();
+            this.result2 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(23, 197);
+            this.textBox1.Location = new System.Drawing.Point(23, 184);
+            this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(257, 22);
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(365, 65);
             this.textBox1.TabIndex = 0;
             // 
             // label1
@@ -60,81 +63,97 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(20, 162);
+            this.label2.Location = new System.Drawing.Point(20, 145);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(121, 17);
+            this.label2.Size = new System.Drawing.Size(106, 17);
             this.label2.TabIndex = 2;
-            this.label2.Text = "Encrypt message:";
+            this.label2.Text = "Text to encrypt:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(20, 281);
+            this.label3.Location = new System.Drawing.Point(20, 297);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(122, 17);
+            this.label3.Size = new System.Drawing.Size(106, 17);
             this.label3.TabIndex = 3;
-            this.label3.Text = "Decrypt message:";
+            this.label3.Text = "Text to decrypt:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(20, 47);
+            this.label4.Location = new System.Drawing.Point(20, 46);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(36, 17);
+            this.label4.Size = new System.Drawing.Size(48, 17);
             this.label4.TabIndex = 4;
-            this.label4.Text = "Key:";
+            this.label4.Text = "Key: 0";
             // 
             // trackBar1
             // 
-            this.trackBar1.Location = new System.Drawing.Point(23, 86);
-            this.trackBar1.Maximum = 26;
-            this.trackBar1.Minimum = -26;
+            this.trackBar1.Location = new System.Drawing.Point(23, 77);
+            this.trackBar1.Maximum = 25;
+            this.trackBar1.Minimum = -25;
             this.trackBar1.Name = "trackBar1";
-            this.trackBar1.Size = new System.Drawing.Size(163, 56);
+            this.trackBar1.Size = new System.Drawing.Size(365, 56);
             this.trackBar1.TabIndex = 5;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(23, 334);
+            this.textBox2.Location = new System.Drawing.Point(23, 341);
+            this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(257, 22);
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(365, 65);
             this.textBox2.TabIndex = 6;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(23, 240);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 7;
-            this.button1.Text = "Encrypt";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(497, 161);
+            this.label5.Location = new System.Drawing.Point(497, 145);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(52, 17);
+            this.label5.Size = new System.Drawing.Size(102, 17);
             this.label5.TabIndex = 8;
-            this.label5.Text = "Result:";
+            this.label5.Text = "Encrypted text:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(497, 297);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(103, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Decrypted text:";
             // 
             // result1
             // 
-            this.result1.AutoSize = true;
-            this.result1.Location = new System.Drawing.Point(500, 197);
+            this.result1.Location = new System.Drawing.Point(500, 184);
+            this.result1.Multiline = true;
             this.result1.Name = "result1";
-            this.result1.Size = new System.Drawing.Size(0, 17);
-            this.result1.TabIndex = 9;
+            this.result1.ReadOnly = true;
+            this.result1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.result1.Size = new System.Drawing.Size(365, 65);
+            this.result1.TabIndex = 11;
+            // 
+            // result2
+            // 
+            this.result2.Location = new System.Drawing.Point(500, 341);
+            this.result2.Multiline = true;
+            this.result2.Name = "result2";
+            this.result2.ReadOnly = true;
+            this.result2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.result2.Size = new System.Drawing.Size(365, 65);
+            this.result2.TabIndex = 12;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(889, 452);
+            this.Controls.Add(this.result2);
             this.Controls.Add(this.result1);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.label4);
@@ -160,9 +179,10 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TrackBar trackBar1;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label result1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox result1;
+        private System.Windows.Forms.TextBox result2;
     }
 }
 
--- Ceasar_Cipher/Form1.cs
+++ Ceasar_Cipher/Form1.cs
@@ -12,7 +12,7 @@ namespace Caesar_Cipher
 {
     public partial class Form1 : Form
     {
-        private int key = 5;
+        private int key;
 
         public Form1()
         {
@@ -21,12 +21,28 @@ namespace Caesar_Cipher
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            textBox1.KeyUp += new KeyEventHandler(TextBox1_KeyPress);
+            textBox2.KeyUp += new KeyEventHandler(TextBox2_KeyPress);
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void TextBox1_KeyPress(object sender, EventArgs e)
+        {
+            ChangeEncryptionResult();
+        }
+
+        private void ChangeEncryptionResult()
         {
             result1.Text = Caesar(textBox1.Text, key);
+        }
+
+        private void TextBox2_KeyPress(object sender, EventArgs e)
+        {
+            ChangeDecryptionResult();
+        }
+
+        private void ChangeDecryptionResult()
+        {
+            result2.Text = Caesar(textBox2.Text, key * -1);
         }
 
         static string Caesar(string value, int shift)
@@ -76,6 +92,19 @@ namespace Caesar_Cipher
                 buffer[i] = letter;
             }
             return new string(buffer);
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            key = trackBar1.Value;
+            label4.Text = "Key: " + key;
+            ChangeEncryptionResult();
+            ChangeDecryptionResult();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
